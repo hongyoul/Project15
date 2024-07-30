647005e0cecff73e8a5d72cc08ffb454a346bfa8
Hong Youl <hongyoul03@gmail.com>
2024-07-30 14:01:17 +0900

    322

---
+++ src/quiz/Quiz4.java
@@ -0,0 +1,21 @@
+package quiz;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Quiz4 {
+
+	public static void main(String[] args) throws IOException {
+	
+		
+		FileOutputStream fos = new FileOutputStream("quiz4.txt");
+		
+		byte[] arr = {65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90};
+		
+		while (true) {
+			
+		}
+
+	}
+
+}
